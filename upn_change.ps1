10fb536565953a36d6146b433519d1d88c9e086a
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-07-30 15:53:48 -0300

    Update upn_change.ps1

--- upn_change.ps1
+++ upn_change.ps1
@@ -1,6 +1,13 @@
+#Get a list with old UPNs
+Connect-AzureAD
+Get-AzureADUser -All $true | ? {$_.UserPrincipalName -like '*old.upn.com'}  | Select DisplayName,ObjectId | Export-Csv -Path D:\aFile.csv
+
+#Change UPNs
 $LocalUsers = Get-ADUser -Filter {UserPrincipalName -like '*old.upn.com'} –SearchBase "OU=CORP,DC=OLD,DC=UPN,DC=COM" -Properties UserPrincipalName -ResultSetSize $null
 $LocalUsers | foreach {$newUpn = $_.UserPrincipalName.Replace("old.upn.com","new.upn.com"); $_ | Set-ADUser -UserPrincipalName $newUpn}
 
-#Check if the attributed syncronized
-Connect-AzureAD
+#Check if the attribute syncronized
 Get-AzureADUser -All $true | ? {$_.UserPrincipalName -like '*new.upn.com'}  | Select DisplayName,UserPrincipalName,ObjectId | Export-Csv -Path D:\file.csv
+
+#Revoke old auth tokens so users are forced to use new UPN in previouly logged devices (and potentially avoid file sharing problems in Teams/OneDrive)
+Revoke-AzureADUserAllRefreshToken -ObjectId 111111aaa2222bbbb3333ccccc444444
