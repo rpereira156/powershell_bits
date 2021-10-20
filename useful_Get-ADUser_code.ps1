2ae4c73cb82c9d09898a1f1bc29062b2c15f26f2
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-10-20 18:04:58 -0300

    Update useful_Get-ADUser_code.ps1

--- useful_Get-ADUser_code.ps1
+++ useful_Get-ADUser_code.ps1
@@ -1,2 +1,5 @@
 # Get a list of users filtered by OU with mail and departmentNumber attributes. Strips out the header
 $var = Get-ADUser -Filter * -SearchBase "OU=" -Properties * | Select-Object mail,@{N='Departmentnumber';E={$_.Departmentnumber[0]}} | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1 | Out-File -FilePath C:\temp\batch_ALL.csv
+
+#Get a list of all users in the domain with the lastLogon attribute
+Get-ADUser -Filter * -Properties Name,LastLogon,Displayname, EmailAddress, Title | select Name,@{Name=’LastLogon’;Expression={[DateTime]::FromFileTime($_.LastLogon)}},DisplayName, EmailAddress, Title | Export-CSV “C:\temp\Email_Addresses.csv”
