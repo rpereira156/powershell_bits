38667f7e8f2989022549ef183bed7d1c3285d3d4
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-11-05 19:45:23 -0300

    Update useful_Get-ADUser_code.ps1

--- useful_Get-ADUser_code.ps1
+++ useful_Get-ADUser_code.ps1
@@ -5,4 +5,5 @@ $var = Get-ADUser -Filter * -SearchBase "OU=" -Properties * | Select-Object mail
 Get-ADUser -Filter * -Properties Name,LastLogon,Displayname, EmailAddress, Title | select Name,@{Name=’LastLogon’;Expression={[DateTime]::FromFileTime($_.LastLogon)}},DisplayName, EmailAddress, Title | Export-CSV “C:\temp\Email_Addresses.csv”
 
 # Get a list of all users in the domain using lastLogonDate (this attribute search on both lastLogon and lastLogonTimestamp and shows up the most recent)
-Get-ADUser -Filter {((Enabled -eq $true))} -Properties LastLogonDate | select samaccountname, Name, lastLogonTimestamp | Sort-Object lastLogonTimestamp | Export-CSV "C:\temp\users_lasLogonDate.csv"
+#Get-ADUser -Filter {((Enabled -eq $true))} -Properties LastLogonDate | select samaccountname, Name, lastLogonTimestamp | Sort-Object lastLogonTimestamp | Export-CSV "C:\temp\users_lasLogonDate.csv"
+Get-ADUser -Filter * -ResultPageSize 0 -Prop CN,lastLogonTimestamp | Select CN,@{n="lastLogonDate";e={[datetime]::FromFileTime($_.lastLogonTimestamp)}} | Export-CSV -NoType "C:\temp\users_last.csv"
