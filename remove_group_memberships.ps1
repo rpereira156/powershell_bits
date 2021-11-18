2fd9848d14bd42c40f92fe48864b6705498a1022
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-11-18 15:33:53 -0300

    Create remove_group_memberships.ps1

---
+++ remove_group_memberships.ps1
@@ -0,0 +1,17 @@
+#Source https://serverfault.com/questions/601813/powershell-finding-all-of-users-group-memberships-and-kicking-it-out-of-them
+#Removes user from all groups that he is a member of.
+
+# 1. Retrieve the user in question:
+$User = Get-ADUser "username" -Properties memberOf
+
+# 2. Retrieve groups that the user is a member of
+$Groups = $User.memberOf |ForEach-Object {
+    Get-ADGroup $_
+} 
+
+# 3. Go through the groups and remove the user
+$Groups | ForEach-Object { Remove-ADGroupMember -Identity $_ -Members $User -Confirm:$false}
+
+# If you want to log every group membership you remove, just for the sake of easy recovery.
+#$LogFilePath = "C:\BackupLocation\user_" + $User.ObjectGUID.ToString() + ".txt"
+#Out-File $LogFilePath -InputObject $(User.memberOf) -Encoding utf8
