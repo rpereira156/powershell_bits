b327a10c589b2b1743cc7fd72eebdf23388eb2c8
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-11-22 19:37:02 -0300

    Update resolve_orphaned_sid.ps1

--- resolve_orphaned_sid.ps1
+++ resolve_orphaned_sid.ps1
@@ -1,3 +1,3 @@
 # From https://itluke.online/2017/12/06/how-to-resolve-orphan-sid-account-name/
 
-Get-ADObject -Filter {objectSid -eq 'S-1-5-21-2646872981-483399078-1445048048-512'} -IncludeDeletedObjects -Properties *
+Get-ADObject -Filter {objectSid -eq 'S-1-2-33-1111111111-4444444444-55555555555-666'} -IncludeDeletedObjects -Properties *
