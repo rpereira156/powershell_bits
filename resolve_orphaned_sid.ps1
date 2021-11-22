028cfb0670f31ed2ab1682c27648d9e6e8dfd857
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-11-22 19:38:40 -0300

    Delete resolve_orphaned_sid.ps1

--- resolve_orphaned_sid.ps1
+++
@@ -1,3 +0,0 @@
-# From https://itluke.online/2017/12/06/how-to-resolve-orphan-sid-account-name/
-
-Get-ADObject -Filter {objectSid -eq 'S-1-2-33-1111111111-4444444444-55555555555-666'} -IncludeDeletedObjects -Properties *
