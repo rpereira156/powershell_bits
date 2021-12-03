1096df9486e4e89ccb575f89c0b92ac5d278b0eb
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2021-12-03 15:08:25 -0300

    Update export_groups.ps1

--- export_groups.ps1
+++ export_groups.ps1
@@ -3,5 +3,8 @@
 # Source https://techtips.tv/windows/powershell/powershell-export-all-active-directory-groups-to-csv/
 # Source https://stackoverflow.com/questions/21518906/list-all-groups-and-their-descriptions-for-a-specific-user-in-active-directory-u
 
-$file = "C:\temp\groupList.csv"
-Get-ADGroup -Filter * -Properties * | Select-Object samAccountName, DistinguishedName, description | Export-Csv -NoTypeInformation -LiteralPath $file
+Get-ADGroup -Properties samAccountName, DistinguishedName, description, Members -Filter '*' |
+    Select-Object -Property samAccountName, DistinguishedName, description , @{
+        Name       = 'MemberCount'
+        Expression = {$_.Members.Count}
+    } | Export-Csv -Path 'C:\temp\groupList.csv'
