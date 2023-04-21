545784230337091e8842b1d75a4cc41c089e22ad
rpereira156 <54335326+rpereira156@users.noreply.github.com>
2023-04-21 14:35:30 +0200

    Update get_sam_from_displayName.ps1

--- get_sam_from_displayName.ps1
+++ get_sam_from_displayName.ps1
@@ -1,3 +1,3 @@
 # Source https://stackoverflow.com/questions/56484733/get-samaccountname-from-display-name-into-csv
-Import-Csv C:\Scripts\displaynames.csv | ForEach {Get-ADUser -Filter "DisplayName -eq '$($_.DisplayName)'" -Properties Name, SamAccountName, City, mail, DistinguishedName | 
-Select Name,SamAccountName, City, mail, DistinguishedName} | Export-CSV -path C:\output\paininthebut.csv -NoTypeInformation
+Import-Csv C:\Scripts\displaynames.csv | ForEach {Get-ADUser -Filter "DisplayName -eq '$($_.DisplayName)'" -Properties Name, SamAccountName, mail, DistinguishedName | 
+Select Name,SamAccountName, mail, DistinguishedName} | Export-CSV -path C:\output\file.csv -NoTypeInformation
